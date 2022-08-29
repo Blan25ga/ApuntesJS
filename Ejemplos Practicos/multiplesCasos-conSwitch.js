a0b861900c8d2ec9f15d1980f7faf90cd80198b9
Blan25ga <gabriel.bl@hotmail.com.ar>
2022-08-29 17:47:20 -0300

    multiples casos con switch

---
+++ Ejemplos Practicos/multiplesCasos-conSwitch.js
@@ -0,0 +1,28 @@
+/*
+switch con multiples casos
+*/
+function clasificarNumeros(valor) {
+  var numeros;
+  switch (valor) {
+    case 1:
+      numeros = "Bajo";
+      break;
+    case 2:
+    case 3:
+      numeros = "Medio"; // si el valor es 2 o 3 se ejecuta este caso.
+      break;
+    case 4:
+    case 5:
+    case 6:
+      numeros = "Alto";
+      break;
+  }
+  return numeros;
+}
+console.log("El numero es: " + clasificarNumeros(1)); // El numero es: Bajo
+console.log("El numero es: " + clasificarNumeros(2)); // El numero es: Medio
+console.log("El numero es: " + clasificarNumeros(3)); // El numero es: Medio
+console.log("El numero es: " + clasificarNumeros(4)); // El numero es: Alto
+console.log("El numero es: " + clasificarNumeros(5)); // El numero es: Alto
+console.log("El numero es: " + clasificarNumeros(6)); // El numero es: Alto
+
