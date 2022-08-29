914b2547e37c829b28fb3b326507c37b5aef1e98
Blan25ga <gabriel.bl@hotmail.com.ar>
2022-08-29 17:28:38 -0300

    sentencias switch

---
+++ Ejemplos Practicos/sentencia switch.js
@@ -0,0 +1,69 @@
+
+// Sentencias switch.
+//Switch permite evaluar una expresión y ejecutar una sentencia dependiendo del valor de la expresión.
+// siempre finalizar cada caso con la sentencia break, para que no se siga ejecutando el resto de los casos.
+
+// Ejemplo 1.
+
+function clasicarValor(valor) {
+
+  var respuesta;
+
+  switch (valor) { // Se evalúa el parametro valor.
+
+    case 1: // Si el valor es 1.
+      respuesta = "El valor es 1";
+      break; // Se sale del switch.
+
+    case 2: // Si el valor es 2.
+      respuesta = "El valor es 2";
+      break; // Se sale del switch.
+
+    case 3: // Si el valor es 3.
+      respuesta = "El valor es 3";
+      break; // Se sale del switch.
+
+    default: // Si el valor no es ninguno de los anteriores.
+      respuesta = "El valor no es 1, 2 o 3";
+      break; // Se sale del switch.
+
+  }
+
+  return respuesta; // Se devuelve la respuesta. se puede usar console.log(respuesta) para ver el resultado en la consola.
+}
+
+clasicarValor(1); // El valor es 1
+clasicarValor(2); // El valor es 2
+clasicarValor(3); // El valor es 3
+clasicarValor(4); // El valor no es 1, 2 o 3
+
+
+// Ejemplo 2.
+
+var prodcto = "Hamburguesa";
+
+switch (prodcto) { // Se evalúa el parametro prodcto.
+  case "Hamburguesa": // Si el valor es "Hamburguesa".
+    console.log("El precio de la hamburguesa es de 10€");
+    break; // Se sale del switch.
+
+  case "Pizza": // Si el valor es "Pizza".
+    console.log("El precio de la pizza es de 8€");
+    break; // Se sale del switch.
+
+  case "Perrito Caliente": // Si el valor es "Perrito Caliente".
+    console.log("El precio del perrito caliente es de 5€");
+    break; // Se sale del switch.
+
+  default: // Si el valor no es ninguno de los anteriores.
+    console.log("No tenemos ese producto");
+    break; // Se sale del switch.
+
+}
+console.log("Gracias por su compra"); // Se ejecuta siempre, fuera del switch.
+
+
+
+
+
+
---
+++ Ejemplos Practicos/switchXdefecto.js
@@ -0,0 +1,37 @@
+/*
+Opcion predetermeinada con switch
+*/
+
+function seleccionarIdioma(valor) {
+  var idioma;
+  switch (valor) {
+    case "1":
+      console.log("Hola Mundo");
+      break;
+    case "2":
+      console.log("Español");
+      break;
+    case "3":
+      console.log("Frances");
+      break;
+    case "4":
+      console.log("Chino");
+      break;
+    case "5":
+      console.log("Aleman");
+      break;
+    default:  // opcion predterminada (default)
+      console.log("Ingles");
+      break;
+  }
+  return idioma;
+}
+
+seleccionarIdioma("0"); // Ingles, por defecto
+seleccionarIdioma("1"); // Hola Mundo
+seleccionarIdioma("2"); // Español
+seleccionarIdioma("3"); // Frances
+seleccionarIdioma("4"); // Chino
+seleccionarIdioma("5"); // Aleman
+seleccionarIdioma("6"); // Ingles, por defecto
+
