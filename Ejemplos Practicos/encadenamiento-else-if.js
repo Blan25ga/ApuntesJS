e2be8bcb4cfd550c40abcdcaf892a55606e26ae3
Blan25ga <gabriel.bl@hotmail.com.ar>
2022-08-29 16:10:38 -0300

    condicionales ese-if

---
+++ Ejemplos Practicos/encadenamiento-else-if.js
@@ -0,0 +1,37 @@
+/* 
+Encadenamiento de sentncias 
+  "if" y "else"
+*/
+
+// Ejemplo 1
+
+function interpretarIMC(masaMuscular) {
+  if (masaMuscular < 18.5) {
+    return "Bajo peso";
+  } else if (masaMuscular <= 25) {
+    return "Peso normal";
+  } else if (masaMuscular <= 30) {
+    return "Sobrepeso";
+  } else {
+    return "Obesidad";
+  }
+}
+
+console.log(interpretarIMC(18.4)); // Bajo peso //*llamada a la funcion por consola
+console.log(interpretarIMC(18.5)); // Peso normal 
+console.log(interpretarIMC(24.9)); // Peso normal
+console.log(interpretarIMC(25)); // Peso normal
+interpretarIMC(25.1); // Sobrepeso //*llamada a la función
+interpretarIMC(29.9); // Sobrepeso
+interpretarIMC(35); // Obesidad, 
+
+
+
+
+
+
+
+// Ejemplo 2
+
+
+
---
+++ Ejemplos Practicos/ordeneLogicoElse-if.js
@@ -0,0 +1,39 @@
+/*
+Condiciones de orden logicos
+*/
+
+//ejemplo:
+
+function calsificaValor(valor) {
+  if (valor < 5) {                 //si el valor es menor a 5 se ejecuta esta linea
+    console.log("Menor que 5");
+  } else if (valor < 10) {          //si el valor es menor a 10 se ejecuta esta linea
+    console.log("El valor es menor a 10");
+  } else {
+    console.log("Mayor o igual a 10");
+  }
+}
+
+calsificaValor(2); // Menor que 5
+calsificaValor(7); // El valor es menor a 10
+calsificaValor(10); // Mayor o igual a 10
+
+//Ejemplo 2
+
+function clasificarValor2(valor2) {
+  if (valor2 < 5) {                 //si el valor es menor a 5 se ejecuta esta linea
+    console.log("Menor que 5");
+  } else if (valor2 < 10) {          //si el valor es menor a 10 se ejecuta esta linea
+    console.log("El valor es menor a 10");
+  } else if (valor2 < 15) {          //si el valor es menor a 15 se ejecuta esta linea
+    console.log("El valor es menor a 15");
+  } else {
+    console.log("Mayor o igual a 15");
+  }
+}
+
+clasificarValor2(2); // Menor que 5
+clasificarValor2(7); // El valor es menor a 10
+clasificarValor2(10); // El valor es menor a 15
+clasificarValor2(20); // Mayor o igual a 15
+
