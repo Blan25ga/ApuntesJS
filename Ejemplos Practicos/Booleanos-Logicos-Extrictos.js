868572e72653d00a1a04c7031d3979cccb3bf5e5
Blan25ga <gabriel.bl@hotmail.com.ar>
2022-08-29 12:21:45 -0300

    Boleanos, Operadores logicos y estrictos

---
+++ Ejemplos Practicos/Booleanos-Logicos-Extrictos.js
@@ -0,0 +1,78 @@
+// Boleanos // Booleanos
+
+//!los operadores true y false son booleanos, y solo se escriben en minisculas pára que se definan.
+
+//* Comparadores
+
+// > mayor que
+// < menor que
+// >= mayor o igual que
+// <= menor o igual que
+// == igual que
+// === igual que y del mismo tipo
+// != diferente que
+
+//* Operadores logicos
+
+// && y
+// || o
+// ! no
+
+//ejemplos
+
+// > mayor que
+console.log(5 > 3); // true
+console.log(5 > 5); // false
+console.log(5 > 6); // false
+
+// < menor que
+console.log(5 < 3); // false
+console.log(5 < 5); // false
+console.log(5 < 6); // true
+
+// >= mayor o igual que
+console.log(5 >= 3); // true
+console.log(5 >= 5); // true
+console.log(5 >= 6); // false
+
+// <= menor o igual que
+console.log(5 <= 3); // false
+console.log(5 <= 5); // true
+console.log(5 <= 6); // true
+
+// == igual que
+console.log(5 == 3); // false
+console.log(5 == 5); // true
+console.log(5 == '5'); // true 
+console.log(5 == 6); // false
+
+// === igual que y del mismo tipo
+console.log(5 === 3); // false
+console.log(5 === 5); // true
+console.log(5 === '5'); // false
+console.log(5 === 6); // false
+
+// != diferente que
+console.log(5 != 3); // true
+console.log(5 != 5); // false
+console.log(5 != '5'); // false
+console.log(5 != 6); // true
+
+//* Operadores logicos
+
+// && y
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+// || o
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+// ! no
+console.log(!true); // false
+console.log(!false); // true
+
---
+++ Ejemplos Practicos/push-shift(queue).js
@@ -0,0 +1,19 @@
+// permanece en fila. (QUEUE)
+
+/* Definir funcion proximoEnFila que tome un arreglo (arreglo) y un numero (elemento)
+como argumentos. Agrega el numero al final de arreglo y luego elimina el primer elemento del arreglo.
+La funcion proximoEnFila debe retornar el elemento que fue eliminado. */
+
+function proximoEnFila(arreglo, elemento) { //se crea la funcion proximoEnFila, con los parametros arreglo y elemento
+  arreglo.push(elemento); //se agrega el elemento al final del arreglo
+  return arreglo.shift(); //se elimina el primer elemento del arreglo y se retorna el elemento eliminado
+}
+
+let miArreglo = [1, 2, 3, 4, 5]; //se crea el arreglo miArreglo
+console.log('Antes: ' + JSON.stringify(miArreglo)); // antes : [1,2,3,4,5] //*se muestra el arreglo miArreglo por consola
+
+
+console.log(proximoEnFila(miArreglo, 6)); //1 //*se muestra el elemento eliminado por consola
+
+
+console.log('Despues: ' + JSON.stringify(miArreglo));// Despues : [2,3,4,5,6] //*se muestra el arreglo miArreglo por consola
