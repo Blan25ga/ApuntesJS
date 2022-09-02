54b809d8e4dba1a9a3b53cfd9cf1ea924000ec78
Blan25ga <gabriel.bl@hotmail.com.ar>
2022-09-02 16:59:37 -0300

    Objetos en js

--- Ejemplos Practicos/Creacion de objetos.js
+++ Ejemplos Practicos/Creacion de objetos.js
@@ -1,3 +1,18 @@
 
 // Crear Objetos//
 
+// Los Objetos del lenguaje JavaScript son colecciones de propiedades, y una propiedad es una asociación entre un nombre (o clave) y un valor. Un valor de propiedad puede ser una función, que se denomina método del objeto. En otras palabras, los objetos son contenedores de valores, y estos valores pueden ser propiedades o métodos.
+
+// Ejemplo de creacion de objeto 
+
+var miObjeto = {  // Se crea un objeto con la palabra reservada var y el nombre del objeto
+  propiedad1: "Hola",
+  propiedad2: "Mundo",
+  propiedad3: 123,
+  propiedad4: true,
+  propiedad5: [1, 2, 3, 4, 5],
+  propiedad6: { nombre: "Juan", apellido: "Perez" },
+  propiedad7: function () {
+    return "Hola Mundo";
+  }
+};
