3a927a33eee854d0f248e666172a79e957b55f33
Blan25ga <gabriel.bl@hotmail.com.ar>
2022-09-07 13:23:50 -0300

    Se agregan plantillas literales y nuevas carpetas o objetos

---
+++ Ejemplos Practicos/Objetos/creacion de forma concisa/forma concisa.js
@@ -0,0 +1,10 @@
+
+// Creacion de objetos de forma concisa
+
+
+// Ejemplo 1
+//* Forma concisa de escribir funcion flecha.
+
+const crearPersona = (nombre, edad, sexo) => ({ nombre, edad, sexo }); // Forma concisa de crear objeto.
+console.log(crearPersona("Juan", 20, "M")); // { nombre: 'Juan', edad: 20, sexo: 'M' }
+
---
+++ Ejemplos Practicos/Plantillas literales ${}/plantillasLiterales.js
@@ -0,0 +1,43 @@
+
+/* Plantillas literales o plantillas de cadenas. 
+
+Características de las plantillas literales:
+-Se usan comillas invertidas, backtick (``), en lugar de comillas simples o dobles.
+-Pueden contener comillas simples y dobles sin necesidad de escaparlas.
+-Las lineas se preservan como se escriben en el codigo,
+-Para reeemplazar una variable o expresion se usa ${variable} o ${expresion}.
+-Dentro de ${} se pueden usar expresiones complejas.
+-Para escribir una plantilla literal en varias lineas se usa el caracter de escape \n.
+
+*/
+
+// Ejemplo 1
+var nombre = "Juan";
+var apellido = "Perez";
+var edad = 30;
+var mensaje = `Hola, mi nombre es ${nombre} ${apellido} y tengo ${edad} años.`;
+
+console.log(mensaje); // Hola, mi nombre es Juan Perez y tengo 30 años.
+
+// Ejemplo 2
+
+const arreglo = [1, 2, 3, 4, 5];
+const mensaje = `El arreglo tiene ${arreglo.length} elementos.`;
+console.log(mensaje); // El arreglo tiene 5 elementos.
+console.log(`El arreglo es ${JSON.stringify(arreglo)}`); // El arreglo es [1,2,3,4,5]
+//* el JSON.stringify() convierte un objeto o valor de JavaScript en una cadena de texto JSON.
+
+
+// Ejemplo 3
+
+// reemplazar el valor de las propiedades de un objeto.
+
+const persona = {
+  nombre: "Juan",
+  apellido: "Perez",
+  edad: 30,
+};
+
+const mensaje = `Hola, mi nombre es ${persona.nombre} ${persona.apellido} y tengo ${persona.edad} años.`;
+console.log(mensaje); // Hola, mi nombre es Juan Perez y tengo 30 años.
+
